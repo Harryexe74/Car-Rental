ac74a511288c7fb00de7874d06c54b53d1232c39
Harryexe74 <junaidmahmood141@gmail.com>
2024-11-23 12:12:26 +0500

    lists

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+				<h1 className="text-2xl font-bold text-center">Welcome to My App</h1>
+				<p className="text-gray-500 text-center">
+					This is a simple application styled with Tailwind CSS.
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
